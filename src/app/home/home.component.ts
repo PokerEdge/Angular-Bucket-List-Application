efc3b3b05d973658ca4da11616deaa2649f0021b
Reid <Reid@pokersprout.com>
2018-04-15 20:29:32 -0400

    removeItem() and animate out on click
    removeItem() and animate out on click
    ————————————
    
    Create new removeItem(i) method, add an index set equal to ‘i’ within
    the inline JavaScript within the to-be-animated DOM element, use splice
    array method to remove the clicked on array element.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,14 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
             style({ opacity: 1, transform: 'translateY(0)', offset: 1})
           ]))
+        ]), { optional: true}),
+
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({ opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
+            style({ opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))
         ]), { optional: true})
       ])
     ])
@@ -29,7 +37,7 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['My first life goal', 'I want to climb a mountain', 'Go ice skiing'];
 
   constructor() { }
 
@@ -40,6 +48,11 @@ export class HomeComponent implements OnInit {
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
     this.itemCount = this.goals.length;
   }
 
